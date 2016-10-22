3bde2cce684e023e24c1332c4fbe91d7f09366ac
Liutos <mat.liutos@gmail.com>
2016-10-22 17:29:07 +0800

    Provides a middlewares mechanism
    With the configurations in file config/middlewares, the developers using
    eloquent-mvc are able to define some functions to be called by each
    requests, and can terminate, change requests or send responses back
    earlier, just depends on the needs

--- src/dispatcher/make-handler.lisp
+++ src/dispatcher/make-handler.lisp
@@ -1,10 +1,32 @@
 (in-package #:eloquent.mvc.dispatcher)
 
-(defun call-action (action env)
-  (funcall (symbol-function action) env))
+(defun call-action (action request)
+  (declare (type symbol action))
+  (declare (type eloquent.mvc.request:<request> request))
+  (funcall (symbol-function action) request))
 
-(defun make-handler (router)
-  (lambda (env)
-    (let* ((request (eloquent.mvc.request:env-to-request env))
-           (action (eloquent.mvc.router:get router request)))
+(defun make-action-caller (router)
+  (lambda (request)
+    (let ((action (eloquent.mvc.router:get router request)))
       (call-action action request))))
+
+(defun make-middleware-caller (middlewares action-caller)
+  (declare (type (trivial-types:proper-list symbol) middlewares))
+  (declare (type function action-caller))
+  (reduce #'make-next
+          middlewares
+          :from-end t
+          :initial-value action-caller))
+
+(defun make-next (middleware next)
+  (declare (type symbol middleware))
+  (lambda (request)
+    (declare (type eloquent.mvc.request:<request> request))
+    (funcall (symbol-function middleware) request next)))
+
+(defun make-handler (middlewares router)
+  (let* ((action-caller (make-action-caller router))
+         (middleware-caller (make-middleware-caller middlewares action-caller)))
+    (lambda (env)
+      (let ((request (eloquent.mvc.request:env-to-request env)))
+        (funcall middleware-caller request)))))
--- src/loader/load.lisp
+++ src/loader/load.lisp
@@ -3,12 +3,17 @@
 (defun make-config-path (directory)
   (merge-pathnames "config/eloquent-mvc.ini" directory))
 
+(defun make-middleware-path (directory)
+  (merge-pathnames "config/middlewares" directory))
+
 (defun make-router-path (directory)
   (merge-pathnames "config/router" directory))
 
 (defun load (directory)
   (let ((config-path (make-config-path directory))
+        (middlewares-path (make-middleware-path directory))
         (router-path (make-router-path directory)))
     (let ((config (eloquent.mvc.config:parse config-path))
+          (middlewares (eloquent.mvc.middleware:parse middlewares-path))
           (router (eloquent.mvc.router:parse router-path)))
-      (start-server config (eloquent.mvc.dispatcher:make-handler router)))))
+      (start-server config (eloquent.mvc.dispatcher:make-handler middlewares router)))))
