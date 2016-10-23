edb8c3ed8434527e5a47b27ffaa8c2551c8f0217
Liutos <mat.liutos@gmail.com>
2016-10-23 23:00:47 +0800

    Refactor the 404 mechanism
    First, change the default action's designator from string to symbol
    points to function ELOQUENT.MVC.ROUTER:NOT-FOUND. And second,
    re-implements this function by using the function RESPOND from package
    ELOQUENT.MVC.RESPONSE to suits the convention among middlewares and
    action handlers

--- src/router/get.lisp
+++ src/router/get.lisp
@@ -1,7 +1,5 @@
 (in-package #:eloquent.mvc.router)
 
-(defvar *default-action* "ELOQUENT.MVC.ROUTER::NOT-FOUND")
-
 (defun find-rule (router request)
   (with-slots (rules) router
     (find-if #'(lambda (rule)
@@ -10,12 +8,12 @@
 
 (defun not-found (request)
   (declare (ignore request))
-  '(404
-    ()
-    ()))
+  (eloquent.mvc.response:respond
+   ""
+   :status 404))
 
 (defun get (router request)
   (let ((rule (find-rule router request)))
     (if rule
         (rule-action rule)
-        *default-action*)))
+        'not-found)))
