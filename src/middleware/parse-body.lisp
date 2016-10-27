00455ebb4ecb6b7aca2e6797cd21d83bb95321a4
Liutos <mat.liutos@gmail.com>
2016-10-27 12:55:20 +0800

    Reserves the raw keys when parsing JSON HTTP body

--- src/middleware/parse-body.lisp
+++ src/middleware/parse-body.lisp
@@ -1,10 +1,15 @@
 (in-package #:eloquent.mvc.middleware)
 
+(defun parse-json-body (string-body)
+  (let ((cl-json:*identifier-name-to-key* #'identity)
+        (cl-json:*json-identifier-name-to-lisp* #'identity))
+    (cl-json:decode-json-from-string string-body)))
+
 (defun parse-body (request next &key)
   "Parse the request HTTP body, generate a structured object for further processing, depends on the request CONTENT-TYPE"
   (let ((content-type (eloquent.mvc.request:request-content-type request)))
     (cond ((string= content-type "application/json")
            (let* ((string-body (eloquent.mvc.request:request-string-body request))
-                  (object (cl-json:decode-json-from-string string-body)))
+                  (object (parse-json-body string-body)))
              (setf (eloquent.mvc.request:getextra :body request) object))))
     (funcall next request)))
