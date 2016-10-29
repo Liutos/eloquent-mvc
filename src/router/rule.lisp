a64fab1be710cda8519b3632e72d1ed8308310e4
Liutos <mat.liutos@gmail.com>
2016-10-29 20:34:40 +0800

    Ignore slash at the end of request path

--- src/router/rule.lisp
+++ src/router/rule.lisp
@@ -33,4 +33,14 @@
   (let ((uri-template (cl-ppcre:parse-string uri-template)))
     (etypecase uri-template
       (list (cl-ppcre:scan uri-template request-path-info))
-      (string (eloquent.mvc.prelude:equivalent request-path-info uri-template)))))
+      (string (or (string= request-path-info uri-template)
+                  (and (slash-path-p request-path-info)
+                       (string=-but-last uri-template request-path-info))
+                  (and (slash-path-p uri-template)
+                       (string=-but-last request-path-info uri-template)))))))
+
+(defun slash-path-p (p)
+  (alexandria:ends-with #\/ p))
+
+(defun string=-but-last (long short)
+  (string= long short :end2 (1- (length short))))
