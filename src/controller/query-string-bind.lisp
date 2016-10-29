84de0b2b1f6124c8fb76b9ea87d7ad43d6d2d702
Liutos <mat.liutos@gmail.com>
2016-10-29 19:22:26 +0800

    Default value and type convertion for query string
    The :DEFAULT and :TYPE keyword parameters in the binding form of macro
    ELOQUENT.MVC.CONTROLLER:QUERY-STRING-BIND are for setting default value
    and doing type convertion

--- src/controller/query-string-bind.lisp
+++ src/controller/query-string-bind.lisp
@@ -2,11 +2,21 @@
 
 (defmacro query-string-bind (bindings request &body body)
   (alexandria:with-gensyms (alist)
-    (let ((bindings (mapcar #'(lambda (binding)
-                                (destructuring-bind (var field) binding
-                                  `(,var (eloquent.mvc.prelude:string-assoc ,field ,alist))))
-                            bindings)))
-      `(let* ((,alist (eloquent.mvc.prelude:parse-query-string
-                       (eloquent.mvc.request:request-query-string ,request)))
-              ,@bindings)
-         ,@body))))
+    (labels ((wrap-value (expr &key default type)
+               (when type
+                 (ecase type
+                   (:integer (setf expr (alexandria:with-gensyms (val)
+                                          `(let ((,val ,expr))
+                                             (and ,val (parse-integer ,val))))))))
+               (when default
+                 (setf expr `(or ,expr ,default))))
+             (make-binding (binding)
+               (destructuring-bind (var field &rest args) binding
+                 `(,var ,(apply #'wrap-value
+                                `(eloquent.mvc.prelude:string-assoc ,field ,alist)
+                                args)))))
+      (let ((bindings (mapcar #'make-binding bindings)))
+        `(let* ((,alist (eloquent.mvc.prelude:parse-query-string
+                         (eloquent.mvc.request:request-query-string ,request)))
+                ,@bindings)
+           ,@body)))))
