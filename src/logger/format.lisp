4badb8e678f72741c7762c3371cdef6e6d2957b3
Liutos <mat.liutos@gmail.com>
2016-11-11 17:09:19 +0800

    Insert a timestamp at the beginning of each log

--- src/logger/format.lisp
+++ src/logger/format.lisp
@@ -4,12 +4,22 @@
   (merge-pathnames (concatenate 'string (string-downcase (symbol-name label)) ".log")
                    (log-directory log)))
 
+(defun now ()
+  "Return a string represents the current time, in the format YYYY-MM-DD hh:mm:ss."
+  (local-time:format-timestring
+   nil
+   (local-time:universal-to-timestamp (get-universal-time))
+   :format '((:year 2) "-" (:month 2) "-" (:day 2) " " (:hour 2) ":" (:min 2) ":" (:sec 2) " " :gmt-offset-hhmm)))
+
+;;; EXPORT
+
 (defun format (label control-string &rest format-arguments)
   "Like function CL:FORMAT but output the formatted string into a file named by LABEL."
   (declare (type keyword label))
-  (let ((destination (make-destination label *log*)))
+  (let ((destination (make-destination label *log*))
+        (fmt (concatenate 'string "~A: " control-string)))
     (with-open-file (stream destination
                             :direction :output
                             :if-exists :append
                             :if-does-not-exist :create)
-      (apply #'cl:format stream control-string format-arguments))))
+      (apply #'cl:format stream fmt (now) format-arguments))))
