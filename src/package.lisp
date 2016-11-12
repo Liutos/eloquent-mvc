787a6bc150308fa0350580e499c4acf7965a8895
Liutos <mat.liutos@gmail.com>
2016-11-12 10:36:04 +0800

    Exports symbols from ELOQUENT.MVC package

--- src/package.lisp
+++ src/package.lisp
@@ -1,2 +1,20 @@
 (defpackage #:eloquent.mvc
-  (:use :cl))
+  (:use #:cl)
+  (:shadow #:load)
+  (:export #:load
+           #:make-project
+           #:unload))
+
+(in-package #:eloquent.mvc)
+
+(defun load (&rest args)
+  #.(documentation 'eloquent.mvc.loader:load 'function)
+  (apply #'eloquent.mvc.loader:load args))
+
+(defun make-project (&rest args)
+  #.(documentation 'eloquent.mvc.project:make-project 'function)
+  (apply #'eloquent.mvc.project:make-project args))
+
+(defun unload (&rest args)
+  #.(documentation 'eloquent.mvc.loader:unload 'function)
+  (apply #'eloquent.mvc.loader:unload args))
