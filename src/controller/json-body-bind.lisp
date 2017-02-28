a4f0112c18a55a3214154fb41ea93483b7841c58
Liutos <mat.liutos@gmail.com>
2017-02-28 23:32:49 +0800

    调整json-body-bind的代码结构

--- src/controller/json-body-bind.lisp
+++ src/controller/json-body-bind.lisp
@@ -3,10 +3,12 @@
 (defmacro json-body-bind (bindings request &body body)
   "Bind variables to values in object, which is parsed from JSON HTTP body, by specified keys, and evaluate the body with these bindings"
   (alexandria:with-gensyms (object)
-    (let ((bindings (mapcar #'(lambda (binding)
-                                (destructuring-bind (var key) binding
-                                  `(,var (cdr (assoc ,key ,object :test #'string=)))))
-                            bindings)))
-      `(let* ((,object (eloquent.mvc.request:getextra :body ,request))
-              ,@bindings)
-         ,@body))))
+    (labels ((wrap-value (key)
+               `(eloquent.mvc.prelude:string-assoc ,key ,object))
+             (make-binding (binding)
+               (destructuring-bind (var key &rest args) binding
+                 `(,var ,(apply #'wrap-value key args)))))
+      (let ((bindings (mapcar #'make-binding bindings)))
+        `(let* ((,object (eloquent.mvc.request:getextra :body ,request))
+                ,@bindings)
+           ,@body)))))
