7588eaef90a70e90a14867fc694c429a2b6eda6e
Liutos <mat.liutos@gmail.com>
2017-03-06 22:38:44 +0800

    Fix a bug
    Extract the real Content-Type part from the CONTENT-TYPE field in a
    <REQUEST> instance, exclude the part of a optional charset

--- src/middleware/parse-body.lisp
+++ src/middleware/parse-body.lisp
@@ -1,5 +1,12 @@
 (in-package #:eloquent.mvc.middleware)
 
+(defun get-content-type (request)
+  "Extract the real Content-Type from REQUEST, removing the optional charset part."
+  (check-type request eloquent.mvc.request:<request>)
+  (let ((content-type (eloquent.mvc.request:request-content-type request)))
+    (apply #'values
+           (eloquent.mvc.prelude:split content-type #\;))))
+
 (defun parse-json-body (string-body)
   (let ((cl-json:*identifier-name-to-key* #'identity)
         (cl-json:*json-identifier-name-to-lisp* #'identity))
@@ -10,7 +17,7 @@
 
 (defun parse-body (request next &key)
   "Parse the request HTTP body, generate a structured object for further processing, depends on the request CONTENT-TYPE"
-  (let ((content-type (eloquent.mvc.request:request-content-type request)))
+  (let ((content-type (get-content-type request)))
     (cond ((string= content-type "application/json")
            (let* ((string-body (eloquent.mvc.request:request-string-body request))
                   (object (parse-json-body string-body)))
