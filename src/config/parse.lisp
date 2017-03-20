0675a2e1010f10cf742ab97014914e2868be74b8
Liutos <mat.liutos@gmail.com>
2017-03-20 16:54:17 +0800

    Fill template variables before parsing configuration file
    The configuration file eloquent-mvc.yaml can contain template
    variables, and they would be filled before parsing by the
    CL-YAML:PARSE function. The template engine used is CL-EMB, and now
    only support one variable: :USER-HOMEDIR-PATHNAME, which is the
    pathname of the current user's home directory.

--- src/config/parse.lisp
+++ src/config/parse.lisp
@@ -3,6 +3,8 @@
 (defun parse (filename)
   "Read content from a file specified by FILENAME, parse it and return the parsing result."
   (declare (type pathname filename))
-  (let ((content (cl-yaml:parse filename)))
+  (let* ((env `(:user-homedir-pathname ,(user-homedir-pathname)))
+         (text (cl-emb:execute-emb filename :env env))
+         (content (cl-yaml:parse text)))
     (make-instance '<config>
                    :content content)))
