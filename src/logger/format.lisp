6c8daf858689c190b5460ce9136a21192ec4003d
Liutos <mat.liutos@gmail.com>
2017-03-21 22:37:53 +0800

    Change the output format of access.log
    Remove the leading timestamp column

--- src/logger/format.lisp
+++ src/logger/format.lisp
@@ -16,10 +16,9 @@
 (defun format (label control-string &rest format-arguments)
   "Like function CL:FORMAT but output the formatted string into a file named by LABEL."
   (declare (type keyword label))
-  (let ((destination (make-destination label *log*))
-        (fmt (concatenate 'string "~A: " control-string)))
+  (let ((destination (make-destination label *log*)))
     (with-open-file (stream destination
                             :direction :output
                             :if-exists :append
                             :if-does-not-exist :create)
-      (apply #'cl:format stream fmt (now) format-arguments))))
+      (apply #'cl:format stream control-string format-arguments))))
