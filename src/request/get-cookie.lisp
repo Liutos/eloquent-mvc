5af122f72860d561447e5e18d453b96d4d4afed6
Liutos <mat.liutos@gmail.com>
2017-03-29 14:17:52 +0800

    Returns NIL when invoking GET-HEADER on a request without cookie

--- src/request/get-cookie.lisp
+++ src/request/get-cookie.lisp
@@ -4,6 +4,8 @@
   "Returns the value of FIELD in REQUEST's Cookie."
   (check-type field string)
   (check-type request <request>)
-  (let* ((cookie-string (get-header request "cookie"))
-         (cookie (eloquent.mvc.prelude:parse-cookie-string cookie-string)))
-    (eloquent.mvc.prelude:string-assoc field cookie)))
+  (let ((cookie-string (get-header request "cookie")))
+    (when (null cookie-string)
+      (return-from get-cookie nil))
+    (let ((cookie (eloquent.mvc.prelude:parse-cookie-string cookie-string)))
+      (eloquent.mvc.prelude:string-assoc field cookie))))
