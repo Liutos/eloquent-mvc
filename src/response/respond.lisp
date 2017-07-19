d6e1349d22f80db1b241114d9397b5fddc7f0201
Liutos <mat.liutos@gmail.com>
2017-07-19 23:03:49 +0800

    Refactor the RESPOND-JSON
    From now on, the differences between RESPOND and RESPOND-JSON are:
    1. The RESPOND-JSON forces the Content-Type HTTP header in response to
    "application/json", and 2. The default value of parameter ENCODER is
    CL-JSON:ENCODE-JSON-TO-STRING

--- src/response/respond.lisp
+++ src/response/respond.lisp
@@ -14,12 +14,14 @@
 
 (defun respond-json (body
                      &key
+                       (encoder #'cl-json:encode-json-to-string)
                        (headers '())
                        (status 200))
   "Encoding body into JSON and send response with at least the header Content-Type equals \"application/json\""
   (setf headers
         (override-header headers :content-type "application/json"))
   (respond
-   (cl-json:encode-json-to-string body)
+   body
+   :encoder encoder
    :headers headers
    :status status))
