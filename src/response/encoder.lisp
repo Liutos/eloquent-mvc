6fe286e52f02cd3a87c4be483476d5d03083585a
Liutos <mat.liutos@gmail.com>
2017-07-19 16:34:32 +0800

    Provides a encoder for encoding PLIST into JSON

--- src/response/encoder.lisp
+++ src/response/encoder.lisp
@@ -3,6 +3,10 @@
 (defgeneric make-encoder (encoder)
   (:documentation "Returns a function for encoding a value into a string."))
 
+(defmethod make-encoder ((encoder (eql :json-plist)))
+  "Returns a function for encoding a PLIST into a JSON string."
+  #'cl-json:encode-json-plist-to-string)
+
 (defmethod make-encoder ((encoder (eql :json-plist-list)))
   "Returns a function for encoding a list consist of plist into a JSON string."
   (lambda (list)
