ca12d505a6b57cd50a9a72db4083f1d7462262eb
Liutos <mat.liutos@gmail.com>
2017-08-20 00:39:02 +0800

    Functions reading files should be within io.lisp

--- src/base/io.lisp
+++ src/base/io.lisp
@@ -1,4 +1,8 @@
-(in-package #:eloquent.mvc.prelude)
+(in-package #:eloquent.mvc.base)
+
+(defun read-file-string (file)
+  "Read the entire FILE into a single string."
+  (uiop:read-file-string file))
 
 (defun read-lines (file)
   "Read the whole FILE and split it by newline character, removes the empty lines."
--- src/base/read-file-string.lisp
+++
@@ -1,5 +0,0 @@
-(in-package #:eloquent.mvc.prelude)
-
-(defun read-file-string (file)
-  "Read the entire FILE into a single string."
-  (uiop:read-file-string file))
