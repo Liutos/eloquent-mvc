36a6b69f39033def75a389b8df775d438c7af096
Liutos <mat.liutos@gmail.com>
2018-03-04 15:22:20 +0800

    支持返回的元素类型为plist（默认为alist）

--- mysql.lisp
+++ mysql.lisp
@@ -1,14 +1,25 @@
 (in-package :fw)
 
+(defun make-element (row columns element-type
+                     &key (null-value nil))
+  (ecase element-type
+    (:alist (mapcar #'(lambda (value column)
+                        (cons column (or value null-value)))
+                    row columns))
+    (:plist (mapcan #'(lambda (value column)
+                        (list (intern (string-upcase column) :keyword)
+                              (or value null-value)))
+                    row columns))))
+
 (defun sql-select (&rest args)
   "封装CLSQL:SELECT"
-  (let ((null-value (my-getf args :null-value nil)))
+  (let ((element-type (my-getf args :element-type :alist))
+        (null-value (my-getf args :null-value nil)))
     (multiple-value-bind (rows columns)
         (apply #'clsql:select `(,@args :allow-other-keys t))
       (when (null rows)
         (return-from sql-select nil))
       (mapcar #'(lambda (row)
-                  (mapcar #'(lambda (value column)
-                              (cons column (or value null-value)))
-                          row columns))
+                  (make-element row columns element-type
+                                :null-value null-value))
               rows))))
