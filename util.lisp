acebd161fe2f23bde8357f1ff8cd18dc2d04cfbf
Liutos <mat.liutos@gmail.com>
2018-03-07 22:54:31 +0800

    新增一个便于定义类的宏

--- util.lisp
+++ util.lisp
@@ -1,4 +1,34 @@
+;;; 在这里提供的都是一些除了package.lisp之外不需要依赖这个项目中的其它文件即可实现的功能
 (in-package :fw)
+
+(defmacro define-my-class ((name &rest my-options &key auto-reader auto-initarg) direct-super-classes direct-slots &rest options)
+  "更方便使用的defclass"
+  (declare (ignorable my-options))
+  (labels ((append-initarg (slot-form)
+             (if auto-initarg
+                 (append slot-form (list :initarg (make-initarg-name slot-form)))
+                 slot-form))
+           (append-reader (slot-form)
+             (if auto-reader
+                 (append slot-form (list :reader (make-reader-name slot-form)))
+                 slot-form))
+           (make-initarg-name (slot-form)
+             (let ((slot-name (first slot-form)))
+               (intern (symbol-name slot-name) :keyword)))
+           (make-reader-name (slot-form)
+             (let ((slot-name (first slot-form)))
+               (intern (format nil "~A-OF" (symbol-name slot-name))))))
+    (let ((slots
+           (mapcar #'(lambda (slot)
+                       (unless (listp slot)
+                         (setf slot (list slot)))
+                       (setf slot (append-initarg slot))
+                       (setf slot (append-reader slot))
+                       slot)
+                   direct-slots)))
+      `(defclass ,name ,direct-super-classes
+         ,slots
+         ,@options))))
 
 (defun assoc-string (item alist &key key (after #'cdr))
   (let ((c (assoc item alist :key key :test #'string=)))
